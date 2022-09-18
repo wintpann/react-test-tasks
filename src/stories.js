0176dcc2268bc6ba4885b9518c4a5be1e231419a
Sergey Manshin <vvintpann@gmail.com>
2022-09-18 17:36:02 +0300

    Add 3 task

--- src/stories.js
+++ src/stories.js
@@ -1,4 +1,5 @@
 import { Task1 } from './tasks/1';
 import { Task2 } from './tasks/2';
+import { Task3 } from './tasks/3';
 
-export const stories = { Task1, Task2 };
+export const stories = { Task1, Task2, Task3 };
