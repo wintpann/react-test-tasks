91ac9d7dbd98a48366602880a975302541e473ad
Sergey Manshin <vvintpann@gmail.com>
2022-09-18 13:49:24 +0300

    Initialize project using Create React App

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { StoryBox } from 'storybox-react';
+import 'storybox-react/dist/styles.css';
+import { stories } from './stories';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<StoryBox stories={stories} />);
